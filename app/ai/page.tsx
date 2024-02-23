57b68628c46b8cd69d7551583b342d8c5ce2bdc9
fformall <achaves1997@yahoo.com>
2024-02-22 22:01:21 -0500

    QOL

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -34,7 +34,7 @@ export default function App() {
     setLoading(true)
 
     try {
-      const response = await fetch(`http://localhost:3000/api/llm`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/llm`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
